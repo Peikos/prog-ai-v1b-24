98790849637bc60565940c72dadba9b3d8628985
Brian van der Bijl <peikos@peikos.net>
2024-09-23 14:46:27 +0200

    Nested loops

---
+++ lists.py
@@ -0,0 +1,22 @@
+# Tel, met een dubbele for-loop, het aantal rode pixels.
+pixels = [["Rood",  "Groen", "Blauw", "Groen", "Blauw"],
+          ["Groen", "Rood",  "Rood",  "Rood",  "Blauw"],
+          ["Groen", "Blauw", "Rood",  "Groen", "Blauw"],
+          ["Groen", "Rood",  "Groen", "Rood",  "Blauw"],
+          ["Rood",  "Groen", "Blauw",  "Blauw", "Rood"]]
+
+# Bereken de som van de hoogste waardes per lijst. En wat is het totaal van de lijst met het hoogste totaal? Doe beide met een dubbele for loop.
+meetwaardes = [[1, 5, 8, 7, 2, 3, 6],
+               [10, 11, 12, 3, 4, 5, 6],
+               [0, 0, 0, 0, 0],
+               [5, 0 -5, -7, 0, 0, -3, 6, 4]]
+
+
+# Loop (met een dubbele for loop) nogmaals door de lijst met pixels heen. Stop zodra je 3x dezelfde kleur ben tegengekomen. Print welke kleur dat is.
+
+
+# Maak vak bovenstaande code een functie, waarbij je het getal 3 door een parameter vervangt.
+
+
+
+# Maak een functie die een getal accepteert. Ga dit in een loop net zo lang keer zichzelf doen tot het resultaat groter is dan 1000. Return het eerste getal groter dan 1000.
